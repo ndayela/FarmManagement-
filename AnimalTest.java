2276a34a2e3cbf12a935ac9e809663c0cc399e13
supercharged501 <nan.james501@gmail.com>
2026-05-03 00:25:04 +0200

    Update AnimalTest.java

--- AnimalTest.java
+++ AnimalTest.java
@@ -1,14 +1,34 @@
 package com.farm.animal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import java.sql.Connection;
 
 public class AnimalTest {
     public static void main(String[] args) {
-        Animal cow = new Cattle("C001", "Holstein", "Female", "Healthy", true);
-        Animal sheep = new Sheep("S001", "Merino", "Male", "Sick");
-        Animal chicken = new Poultry("P001", "Broiler", "Female", "Sold");
+
+
+        try {
+            Connection conn = DBConnection.getConnection();
+            System.out.println("✅ Connected to Farm_System successfully!");
+            DBConnection.closeConnection(conn);
+        } catch (Exception e) {
+            System.out.println("❌ Connection failed: " + e.getMessage());
+        }
+
+        Cattle cow = new Cattle("C001", "Holstein", "Female", "Healthy", true);
+        Sheep sheep = new Sheep("S001", "Merino", "Male", "Sick");
+        Poultry chicken = new Poultry("P001", "Broiler", "Female", "Sold");
 
         cow.displayInfo();
         sheep.displayInfo();
         chicken.displayInfo();
+
+
+        cow.saveToDB();
+        sheep.saveToDB();
+        chicken.saveToDB();
     }
 }
 
