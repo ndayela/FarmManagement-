e67a55f66e55f9f7f9a111a3577a7d5e08a44ccd
supercharged501 <nan.james501@gmail.com>
2026-05-03 00:36:18 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
-
+package com.farm.animal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.*;
+import com.microsoft.sqlserver.jdbc.*;
 public class Main {
     public static void main(String[] args) {
 
